86ea5d0fcc53de4e9c2e64b88cc58996e925d881
Marx Alonso <marxchip99@gmail.com>
2024-09-15 15:38:15 -0500

    add Banners general

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,90 @@
+import { motion } from "framer-motion";
+
+interface BannerProps {
+  image: string;
+  title: string;
+  subtitle: string;
+  link: string;
+}
+
+const SlideLeft = (delay: number) => {
+  return {
+    hidden: {
+      opacity: 0,
+      x: 100,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 1,
+        delay: delay,
+      },
+    },
+  };
+};
+
+// Tipifica el componente correctamente usando la interfaz
+export const BannerPrincipal = ({ image, title, subtitle, link }: BannerProps) => {
+  return (
+    <div className="container">
+      <div className="bg-[#f9f9f9] grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 py-14">
+        {/* Banner Image Section */}
+        <div>
+          <img src={image} alt={title} className="w-[300px] md:max-w-[400px] xl:min-w-[600px] h-full object-cover"/>
+        </div>
+        {/* Banner Text Section */}
+        <div className="flex flex-col justify-center text-center md:text-left space-y-4 lg:max-w-[500px]">
+          <h1 className="text-4xl font-bold capitalize font-playfair">{title}</h1>
+          <p className="text-gray-600">{subtitle}</p>
+          <div className="flex justify-center md:justify-start">
+            <button className="primary-btn" ><a href={link}>Explorar Mas</a></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const Banner = () => {
+  return (
+    <div className="container my-14">
+      <div className="bg-[#f9f9f9] py-14 md:py-24 flex justify-center items-center">
+        {/* brand info */}
+        <div className="flex flex-col justify-center text-center space-y-8 lg:px-44">
+          <motion.h1
+            variants={SlideLeft(0.5)}
+            initial="hidden"
+            whileInView={"visible"}
+            viewport={{ once: true }}
+            className="text-3xl lg:text-5xl font-bold uppercase"
+          >
+            Get 20% Discount On Your First Order, Are You Ready To Buy
+          </motion.h1>
+          <motion.p
+            variants={SlideLeft(0.7)}
+            initial="hidden"
+            whileInView={"visible"}
+            viewport={{ once: true }}
+            className=""
+          >
+            We will make sure you get the right and the best quality products
+            for your workout
+          </motion.p>
+          <motion.div
+            variants={SlideLeft(0.9)}
+            initial="hidden"
+            whileInView={"visible"}
+            viewport={{ once: true }}
+            className="flex justify-center gap-6"
+          >
+            <button className="primary-btn">Learn More</button>
+            <button className="border-2 border-primary text-primary font-bold px-5 rounded-lg">
+              Stay In Touch
+            </button>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,24 @@ import { Hero } from "../../components/Hero/Hero";
 import { Navbar } from "../../components/Navbar/Navbar";
 import BgImage from "../../assets/bg.png";
 import { Equipamiento } from "../../components/Equipamiento/Equipamiento";
+import { Banner, BannerPrincipal } from "../../components/Banner/Banner";
+
+import Img1 from "../../assets/2.png"
+import Img2 from "../../assets/3.png"
+const BannerData = {
+  image: Img1,
+  title: "The Importants To Take Care Of Yourself",
+  subtitle: "The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus.",
+  link: "#",
+
+}
+const Banner2Data = {
+  image: Img2,
+  title: "The Importants To Take Care Of Yourself",
+  subtitle: "The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus.",
+  link: "#",
+
+}
 const bgStyle = {
   backgroundImage: `url(${BgImage})`,
   backgroundRepeat: "no-repeat",
@@ -17,6 +35,9 @@ export const HomePage = () => {
         <Hero />
       </div>
       <Equipamiento />
+      <BannerPrincipal {...BannerData} />
+      <BannerPrincipal {...Banner2Data} />
+      <Banner />
     </div>
   );
 };
