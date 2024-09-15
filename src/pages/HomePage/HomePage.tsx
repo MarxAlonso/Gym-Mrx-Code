f8b36d07281984c69587d63f9e3ba221c5bfce20
Marx Alonso <marxchip99@gmail.com>
2024-09-15 00:37:07 -0500

    add Hero in HomePage React

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
+import { Hero } from "../../components/Hero/Hero"
 import { Navbar } from "../../components/Navbar/Navbar"
 
 export const HomePage = () => {
     return(
         <>
         <Navbar />
+        <Hero />
         </>
     )
 }
