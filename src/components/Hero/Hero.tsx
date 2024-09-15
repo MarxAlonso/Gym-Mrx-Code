08255e777f668afd1d817d658cc4bc8de105653d
Marx Alonso <marxchip99@gmail.com>
2024-09-15 15:04:32 -0500

    add Hero finished

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,23 @@
 import { FaPlay } from "react-icons/fa";
-import HeroImg from "../../assets/dumbell.png"
+import HeroImg from "../../assets/dumbell.png";
+import { motion } from "framer-motion";
+
+const SlideRight = (delay: number) => {
+  return {
+    hidden: {
+      opacity: 0,
+      x: -100,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 1,
+        delay: delay,
+      },
+    },
+  };
+};
 export const Hero = () => {
   return (
     <>
@@ -8,26 +26,47 @@ export const Hero = () => {
           {/* Brand Info */}
           <div className="flex flex-col justify-center py-14 md:py-0 font-playfair">
             <div className="text-center md:text-left spacy-y-6">
-              <h1 className="text-5xl lg:text-6xl font-bold leading-relaxed xl:leading-normal">
+              <motion.h1
+                variants={SlideRight(0.6)}
+                initial="hidden"
+                animate="visible"
+                className="text-5xl lg:text-6xl font-bold leading-relaxed xl:leading-normal"
+              >
                 Gym Gives you the perfect{" "}
                 <span className="text-primary">Health</span>
-              </h1>
-              <p className="text-gray-600 xl:max-w-[500px]">
+              </motion.h1>
+              <motion.p
+                variants={SlideRight(1.2)}
+                initial="hidden"
+                animate="visible"
+                className="text-gray-600 xl:max-w-[500px]"
+              >
                 It is a long established fact that a reader will be by readable
                 content of a page when are the best product.
-              </p>
+              </motion.p>
               {/* Seccion de boton */}
-              <div className="flex justify-center items-center gap-8 md:justify-start !mt-4">
-                <button className="primary-btn flex items-center gap-2">Ordenar Ahora</button>
+              <motion.div
+                variants={SlideRight(1.5)}
+                initial="hidden"
+                animate="visible"
+                className="flex justify-center items-center gap-8 md:justify-start !mt-8"
+              >
+                <button className="primary-btn flex items-center gap-2">
+                  Ordenar Ahora
+                </button>
                 <button className="flex justify-center items-center gap-2">
-                    <FaPlay /> Ver Ahora
-                    </button>
-              </div>
+                  <FaPlay /> Ver Ahora
+                </button>
+              </motion.div>
             </div>
           </div>
           {/* Hero Image */}
           <div className="flex justify-center items-center">
-            <img src={HeroImg} alt="" className="w-[350px] md:w-[550px] xl:w-[700px] drop-shadow"/>
+            <img
+              src={HeroImg}
+              alt=""
+              className="w-[350px] md:w-[550px] xl:w-[700px] drop-shadow"
+            />
           </div>
         </div>
       </section>
